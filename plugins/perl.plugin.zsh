5a2912c669ba48eaca82dcabd2c03f6584be0a43
franck cuny <franck@lumberjaph.net>
2010-09-16 16:38:31 +0200

    working perl plugin

--- plugins/perl.plugin.zsh
+++ plugins/perl.plugin.zsh
@@ -1,14 +1,22 @@
-export CATALYST_DEBUG=1
-export PERL_MM_USE_DEFAULT=1
-
 alias pmver="perl -le '\$m = shift; eval qq(require \$m) or die qq(module \"\$m\" is not installed\\n); print \$m->VERSION'"
 
-catalyst_version=`pmver Catalyst::Devel`
-
-if [[ $catalyst_version -gt 0 ]]; then
-    alias cs="perl script/*_server.pl -d"
+if [ -d ~/perl5/perlbrew ]; then
+   source ~/perl5/perlbrew/etc/bashrc
 fi
 
-alias c='cpanm'
+export PERL_MM_USE_DEFAULT=1
 
-source ~/perl5/perlbrew/etc/bashrc
+catalyst_version=`pmver Catalyst::Devel 2>/dev/null`
+if [[ $catalyst_version -gt 0 ]]; then
+   export CATALYST_DEBUG=1
+   alias cs="perl script/*_server.pl -d"
+fi
+
+cpanm_version=`pmver App::cpanminus 2>/dev/null`
+if [[ $cpanm_version -gt 0 ]]; then
+   alias c='cpanm'
+else
+   alias c='cpan'
+fi
+
+
